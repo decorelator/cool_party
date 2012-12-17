f8b5315f69580b09a1d71fa772e4a8ec4f81f487
fdeveloperf <fdeveloperf@gmail.com>
2012-12-17 15:11:38 +0200

    xml modify

---
+++ src/model/Switcher.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Switcher {
+	private int ico;
+	private String caption;
+	private int id;
+	
+	public Switcher(int id, int ico, String caption) {
+		this.ico = ico;
+		this.caption = caption;
+		this.id = id;
+	}
+
+
+	public String getCaption() {
+		return caption;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	
+}
